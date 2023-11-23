9c916d88d49d992b1481fab2c7f53ad573906449
Eddie <callenberg99@gmail.com>
2023-11-23 09:57:37 +0100

    Första delen av uppgiften klar.

--- Ovnings-Uppgift-Tva/Program.cs
+++ Ovnings-Uppgift-Tva/Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<string> shoppingList = new List<string>();
+        bool isRunning = true;
+        while (isRunning)
+        {
+            Console.WriteLine("Hello, User!");
+            Console.WriteLine("1. - Add Items to your Shopping List.");
+            Console.WriteLine("2. - View your Shopping List.");
+            int switchSelector = Convert.ToInt32(Console.ReadLine());
+
+            switch (switchSelector)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Please enter what you would like to add to your shoppinglist.");
+                    string newItem = Console.ReadLine();
+                    if(string.IsNullOrEmpty(newItem))
+                    {
+                        Console.WriteLine("Cannot be empty. Please try again");
+                    }
+                    shoppingList.Add(newItem);
+                    Console.WriteLine($"You added {shoppingList.LastOrDefault()} to the shoppinglist");
+                    break;
+            }
+        }
+        
+
     }
 }
